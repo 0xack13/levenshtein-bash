2f1b8c5a39d3a4b20c228cd616ed3c7575b7003f
lrwx00t <lrwx00t@gmail.com>
2022-03-27 14:50:49 +1100

    levenshtein ..

--- levenshtein.sh
+++ levenshtein.sh
@@ -5,36 +5,71 @@ min_number() {
 }
 
 leven(){
+    # key value pair
+    declare -A matrix
+    t1=$1
+    t2=$2
+    firstlen=${#t1}
+    secondlen=${#t2}
+    num_rows=$(( $secondlen + 1 ))
+    num_columns=$(( $firstlen + 1 ))
 
-
-declare -A matrix
-# num_rows=4
-# num_columns=5
-
-t1=$1 #"hello"
-t2=$2 #"hello1"
-firstlen=${#t1}
-secondlen=${#t2}
-num_rows=$secondlen
-num_columns=$firstlen
-echo $firstlen
-echo $secondlen
-
-for ((i=1;i<=num_rows;i++)) do
-    for ((j=1;j<=num_columns;j++)) do
-        matrix[$i,$j]=$(( $i*$j ))
-        echo -n $matrix[$i,$j]
+    for ((i=1;i<=num_rows;i++)) do
+        for ((j=1;j<=num_columns;j++)) do
+            matrix[$i,$j]=0
+        done
     done
-    echo "\n"
-done
-echo "last:"
-# echo $(expr $firstlen - 1)
-set -x
-tt2=$(( firstlen ))
-tt1=$(( secondlen ))
-echo $tt1 $tt2
-# echo $matrix[0][0]
-# echo $matrix[1,4]
-echo "$matrix[${tt1},${tt2}]"
-# echo "matrix:"
+
+    # init rows
+    for ((i=1;i<=num_rows;i++)) do
+            matrix[$i,1]=$(( i - 1 ))
+    done
+
+    # init columns
+    for ((j=1;j<=num_columns;j++)) do
+        matrix[1,$j]=$(( j - 1 ))
+    done
+
+    for ((i=2;i<=num_rows;i++)) do
+        for ((j=2;j<=num_columns;j++)) do
+            left=$(( $matrix[$(( i - 1 )),$j] + 1 ))
+            down=$(( $matrix[$i,$(( j - 1 ))] + 1 ))
+            left_down=$(( $matrix[$(( i - 1 )),$(( j - 1 ))] ))
+            if [[ $t1[$(( i - 1 ))] != $t2[$(( j - 1 ))] ]]; then
+                left_down=$(( left_down + 1 ))
+            fi
+            minval=$(min_number $left $down $left_down)
+            matrix[$i,$j]=$minval
+        done
+    done
+    echo "$matrix[$num_rows,${num_columns}]"
+}
+
+doyoumean() {
+    declare -a words result1 result2 result3
+    words=("top" "Nitro" "zero" "test4" "cow" "tool" "fool" "cool")
+    for w in ${words[*]}
+    do     
+        r=$(leven $w $1)
+        case $r in
+        0)
+            return
+            ;;
+        1)
+            result1+=($w)
+            ;;
+
+        2)
+            result2+=($w)
+            ;;
+
+        3)
+            result3+=($w)
+            ;;
+        esac
+    done
+    if (( ${#result1[@]} )) || (( ${#result2[@]} )) || (( ${#result3[@]} )); then
+        echo "Did you mean?"
+        echo $result1 $result2 $result3
+    fi
 }
